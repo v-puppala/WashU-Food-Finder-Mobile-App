2efe2c395317b603c05f88de2035f8f881c30dd4
Micah Rounds <mjrounds@wustl.edu>
2020-07-21 01:49:08 -0500

    update time format on detailview

--- MattKusak-VishalPuppala-MicahRounds-Final-Project/EventDetailView.swift
+++ MattKusak-VishalPuppala-MicahRounds-Final-Project/EventDetailView.swift
@@ -17,7 +17,7 @@ class EventDetailView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let df = DateFormatter()
-        df.dateFormat = "E d MMM y, HH:mm a"
+        df.dateFormat = "E d MMM y, h:mm a"
         titleLabel.text = detailLoc.title
         locLabel.text = detailLoc.locationName
         endDateLabel.text = "Lasts Until: "+df.string(from: detailLoc.date)
